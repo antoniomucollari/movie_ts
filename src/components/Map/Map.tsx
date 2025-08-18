8b275c4f468c1e8c850dc99c89ef25cb05e190df
antoniomucollari <mucollariantonio@gmail.com>
2025-08-18 13:50:35 +0200

    fixing marker icon not showing

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,14 @@
 import {MapContainer, Marker, Popup, TileLayer, useMapEvent} from "react-leaflet";
 import type Coordinate from "./coordinate.model.ts";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import type {LatLngTuple} from "leaflet";
+import type { Marker as LeafletMarker } from "leaflet";
 
 export default function Map(props: MapProps){
     const [coordinates, setCoordinates] = useState(props.coordinates);
     const first = coordinates?.[0];
     const CordinateArr: LatLngTuple | undefined = first ? [first.lat, first.lng] : undefined;
-
+    const markerRef = useRef<LeafletMarker>(null);
     return (
         <MapContainer center={CordinateArr?? [41.32853580329582, 19.82266119258898]} zoom={12} scrollWheelZoom={true} style={{height:'500px'}}>
             <TileLayer attribution="React Movies" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
@@ -18,7 +19,7 @@ export default function Map(props: MapProps){
                     props.CoordinateProp(cordi)
                 }
             }} /> : undefined}
-            {coordinates?.map((coordinate) => <Marker position={[coordinate.lat, coordinate.lng]} key={coordinate.lat + coordinate.lng}>
+            {coordinates?.map((coordinate) => <Marker ref={markerRef}  position={[coordinate.lat, coordinate.lng]} key={coordinate.lat + coordinate.lng}>
                 {coordinate.message ?  <Popup> {coordinate.message}</Popup> : undefined}
             </Marker>)}
         </MapContainer>
